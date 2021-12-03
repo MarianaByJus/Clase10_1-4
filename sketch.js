2f96c923423a430a12451f320c0a146cfc631f99
MarianaByJus <86327356+MarianaByJus@users.noreply.github.com>
2021-12-03 12:46:29 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-
+var trex, trex_running
 
 
 function preload(){
-
+trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
 
 }
 
@@ -10,6 +10,8 @@ function setup(){
   createCanvas(600,200)
   
   //crear sprite del t-rex.
+  trex = createSprite(200,200,20,10);
+  trex.addAnimation("running",trex_running);
    
 }
 
